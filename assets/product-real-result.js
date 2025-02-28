940fe0953ee49d9eae06d653bae74b6b24fdaab9
dev02615 <psky0591@gmail.com>
2025-02-28 11:53:34 +0200

    Completed

--- assets/product-real-result.js
+++ assets/product-real-result.js
@@ -1,24 +1,31 @@
 const result_item = document.querySelectorAll(".result-item");
 const point_ele = document.querySelectorAll(".result-item-point");
+const result_bottom_content = document.querySelectorAll(
+  ".result-bottom-content"
+);
 
 point_ele.forEach((ele) => {
   ele.addEventListener("click", function () {
     const content_ele = this.nextElementSibling.lastElementChild;
-    console.log(content_ele.scrollHeight);
+    const rbc = result_bottom_content[parseInt(this.dataset.index) - 1];
 
     if (!this.classList.contains("active")) {
-      point_ele.forEach((ele) => {
+      point_ele.forEach((ele, index) => {
         ele.classList.remove("active");
         ele.innerHTML = "+";
+        result_bottom_content[index].style.height = 0;
         ele.nextElementSibling.lastElementChild.style.height = 0;
       });
-      this.classList.add("active");
-      content_ele.style.height = content_ele.scrollHeight + "px";
+
       this.innerHTML = "-";
+      this.classList.add("active");
+      rbc.style.height = rbc.scrollHeight + "px";
+      content_ele.style.height = content_ele.scrollHeight + "px";
     } else {
-      this.classList.remove("active");
-      content_ele.style.height = 0;
       this.innerHTML = "+";
+      rbc.style.height = 0;
+      content_ele.style.height = 0;
+      this.classList.remove("active");
     }
   });
 });
